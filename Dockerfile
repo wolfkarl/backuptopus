637e4537197f505939f85adabba59c82333a55cc
Karl Wolf <wolfkarl@gmail.com>
2025-09-02 22:34:19 +0200

    postgres dump

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM python:3.11.2
 COPY --from=docker.io/astral/uv:latest /uv /uvx /bin/
+RUN apt-get update && apt install postgresql-client -y
 
 WORKDIR /app/pybackup
 
--- main.py
+++ main.py
@@ -44,6 +44,22 @@ def do_backup():
         else:
             logging.warning(f"Target path does not exist: {target_path}")
 
+        # connect to psql and dump databases
+    for postgres_connection_string in PG_TARGTETS:
+        if not postgres_connection_string:
+            continue
+        db_name = postgres_connection_string.split("/")[-1]
+        dump_file = f"{tmpdir}/{db_name}_backup_{timestamp}.sql"
+        logging.debug(f"Running command: pg_dump {postgres_connection_string} > {dump_file}")
+        os.system(f"pg_dump {postgres_connection_string} > {dump_file}")
+        logging.info(f"Dumped database {db_name} to {dump_file}")
+        if os.path.exists(dump_file):
+            dump_size = os.path.getsize(dump_file)
+            human_dump_size = "{:.2f} MB".format(dump_size / (1024 * 1024)) if dump_size > 1024 * 1024 else "{:.2f} KB".format(dump_size / 1024)
+            logging.info(f"Dump file size for {db_name}: {human_dump_size}")
+        else:
+            logging.warning(f"Dump file not found for {db_name}: {dump_file}")
+
     # create tar.gz archive
     logging.info("Creating tar.gz archive...")
     archive_name = f"{BACKUP_DIR}/backup_{timestamp}.tar.gz"
@@ -54,9 +70,14 @@ def do_backup():
     archive_size = os.path.getsize(archive_name)
     human_size = "{:.2f} MB".format(archive_size / (1024 * 1024)) if archive_size > 1024 * 1024 else "{:.2f} KB".format(archive_size / 1024)
     logging.info(f"Archive size: {human_size}")
+
     # clean up temporary directory
     os.system(f"rm -rf {tmpdir}")
     logging.info(f"Cleaned up temporary directory: {tmpdir}")
+
+
+
+
 
 if __name__ == "__main__":
     main()
